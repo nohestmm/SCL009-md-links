c8299c3dc3e68414004e2748b110ae57f3e11786
nohestmm <nohestmm@gmail.com>
2019-06-22 21:29:41 -0400

    subiendo captura del array con process tomado de la terminal

--- index.js
+++ index.js
@@ -3,44 +3,51 @@
 // };
 
 
-const process = require('process');
-const marked = require ('marked');
-const fs = require ('fs');
-const path = require ('path');
+const process = require('process'); 
+let arrayTerminal = [];
+// const marked = require ('marked');
+// const fs = require ('fs');
+// const path = require ('path');
 
 
-const array =(path) =>{
-let array = [];
-fs.readFile(path,"utf-8", (error, data) =>{
-if (error)
-throw error;
-const renderer = new marked.Renderer();
-renderer.link =(href,title,text) =>{
-  array.push({
-    href:href,
-    text:text,
-    file:path
-  });
-}
- marked(data, {renderer:renderer});
-    console.log(array);
-  });
+// const array =(path) =>{
+// let array = [];
+// fs.readFile(path,"utf-8", (error, data) =>{
+// if (error)
+// throw error;
+// const renderer = new marked.Renderer();
+// renderer.link =(href,title,text) =>{
+//   array.push({
+//     href:href,
+//     text:text,
+//     file:path
+//   });
+// }
+//  marked(data, {renderer:renderer});
+//     console.log(array);
+//   });
  
-}
-console.log(array("./prueba.md"));
+// }
+// console.log(array("./prueba.md"));
 
 process.argv.forEach((val, index) => {
-  console.log(`${index}: ${val}`);
-  
+   arrayTerminal.push (process.argv[index]);
+   console.log(`${index}: ${val}`);
+   
+     
 });
+console.log(arrayTerminal);
+console.log(arrayTerminal[2]);
 
-const FileHound = require('filehound');
+
+
+
+
+// const files = FileHound.create()
+//   .paths("/home/laboratoria/Desktop/SCL009-md-links")
+//   .ext('md')
+//   .find();
  
-const files = FileHound.create()
-  .paths("/home/laboratoria/Desktop/SCL009-md-links")
-  .ext('md')
-  .find();
- 
-files.then(console.log);
+// files.then(console.log);
 
 
