420dd3ee4c10797e18249bdbf1ce81e5bc515b59
nohestmm <nohestmm@gmail.com>
2019-06-29 09:37:21 -0400

    subiendo funcionalidad de la funcion md links luego de capturar datos desde indes.js

--- index.js
+++ index.js
@@ -6,18 +6,10 @@
 let c = console.log;
 let options = [];
 let arrayTerminal = [];
-let File = [];
-const marked = require ('marked');
-const fs = require ('fs');
+let arrayLinks = [];
+
 const process = require('process'); 
-const FileHound = require ('filehound');
-const path = require ('path');
-const fetch = require('node-fetch');
 const mdLinks= require('./mdLinks');
-
-c("****************Bienvenidos*********************");
-c("******Coloca la ruta o archivo a examinar*******");
-
 
 process.argv.forEach((val, index) => {
    arrayTerminal.push(process.argv[index]);
@@ -25,157 +17,183 @@ process.argv.forEach((val, index) => {
    });
 c(arrayTerminal);
 
+//c(arrayTerminal[2]);
+// fs.stat(arrayTerminal[2],(error, stats) =>{
+
+//    if (error){ 
+//       c(error);
+//    }
+   
+//    if (stats.isFile()){
+//    c("**************************************************");
+//    c("*                   Soy un Path                  *");
+//    c("**************************************************");
+   
+   
+   
+//    // llamar a la funcion para imprimir los links en la consola
+//    arrayFile(arrayTerminal[2])
+//    .then(res =>{
+//       if (options.length === 0){
+//          //console log para ver los links
+//          //c(File);
+//          arrayLinks.forEach((el, index)=> {
+            
+//             c(`${index}: href: ${el.href}`);
+//             c(`   texto: ${el.text}`);
+//             c(`   file: ${el.file}`);
+         
+//          });
+//          }
+//          //funcion que muestra los link ok para validate o stats
+//          if (options.length>0){
+//                  fetchlinks(arrayLinks);
+//                }
+//    })
+//    .catch(error=> c(error));
+
+//    }
+//    if (stats.isDirectory()){
+//    c("***************************************************");
+//    c("*                Soy Un Directorio                *");
+//    c("***************************************************");
+//    searchfileinDirectory(arrayTerminal[2]);
+//    }
+//    });
+
 if (arrayTerminal[3] && arrayTerminal[4]){
    if ((arrayTerminal[3] === "--validate" && arrayTerminal[4] === "--stats" ) || (arrayTerminal[3] === "--v" && arrayTerminal[4] === "--s" ) ){
-      options.push({
-      validate:true},
-      {stats:true
-      });
-   }
+      options.push(
+      {validate:true},
+      {stats:true}
+      );
+ }
    if ((arrayTerminal[3] === "stats" && arrayTerminal[4] === "validate") || (arrayTerminal[3] === "--s" && arrayTerminal[4] === "--v" )){
-      options.push({
-      stats:true},
-      {validate:true
-      });
+      options.push(
+      {stats:true},
+      {validate:true}
+      );
      }
     }
 else if (arrayTerminal[3]){
 if (arrayTerminal[3] === "validate" || arrayTerminal[3] === "--v"){
-   options.push({
-   validate:true
-   });
+   options.push(
+   {validate:true}
+   );
 }
 if (arrayTerminal[3] === "stats"  || arrayTerminal[3] === "--s"){
-   options.push({
-   stats:true
-   });
+   options.push(
+   {stats:true}
+   );
 }
+
 }
-c(options);
-c(options.length);
 
-
-
-mdLinks(5,options)
-  .then(res => {
+mdLinks(arrayTerminal[2],options);
+//   .then(res => {
   
-  })
-  .catch(error => c(error));
-
-//c(arrayTerminal[2]);
-fs.stat(arrayTerminal[2],(error, stats) =>{
-
-if (error){ 
-   c(error);
-   
-}
+//   })
+//   .catch(error => c(error));
 
 
-if (stats.isFile()){
-c("**************************************************");
-c("*                   Soy un Path                  *");
-c("**************************************************");
-
-
-
-// llamar a la funcion para imprimir los links en la consola
-arrayFile(arrayTerminal[2]);
-}
-if (stats.isDirectory()){
-c("***************************************************");
-c("*                Soy Un Directorio                *");
-c("***************************************************");
-searchfileinDirectory(arrayTerminal[2]);
-}
-});
   
 
 
 
-//cambiar readfile a promesas
-const arrayFile =(path) =>{
-return new Promise ((resolved, rejected) => {
-fs.readFile(path,"utf-8", (error, data) =>{
-if (error){
-rejected (error);
+// //cambiar readfile a promesas
+// const arrayFile =(path) =>{
+// return new Promise ((resolved, rejected) => {
+// fs.readFile(path,"utf-8", (error, data) =>{
+// if (error){
+// rejected (error);
 
-}
-else{
-resolved(data);
-const renderer = new marked.Renderer();
-renderer.link =(href,title,text) =>{
-File.push({
-href:href,
-text:text,
-file:path
-});
-}
-marked(data, {renderer:renderer});
-//condicional para imprimir sin options
-if (options.length === 0){
-//console log para ver los links
-//c(File);
-File.forEach((el, index)=> {
+// }
+// else{
+
+// const renderer = new marked.Renderer();
+// renderer.link =(href,title,text) =>{
+// arrayLinks.push({
+// href:href,
+// text:text,
+// file:path
+// });
+// }
+// marked(data, {renderer:renderer});
+// //condicional para imprimir sin options
+
+// }
+// resolved(arrayLinks);
+// });
+// });
+// }
+
+
+
+// //funcion para imprimir los links ok y no ok
+// const fetchlinks = (arrayLinks) => {
+// let arrayObjectFetch = [];
+// arrayLinks.forEach(el => {
    
-   c(`${index}: href: ${el.href}`);
-   c(`   texto: ${el.text}`);
-   c(`   file: ${el.file}`);
-
-});
-}
-//funcion que muestra los link ok para validate o stats
-//if (options.length>0)
-fetchlinks(File);
-}
-});
-});
-}
-
-
-
-
-//funcion para imprimir los links ok y no ok
-const fetchlinks = (File) => {
-
-File.forEach(el => {
-fetch(el.href)
-    .then(res => {
-       //c(res);
+// fetch(el.href)
+//     .then(res => {
+//        //c(res);
        
-     c(`${res.url} ${res.ok} ${res.status} ${res.statusText}`);
+//      c(`${res.url} ${res.ok} ${res.status} ${res.statusText}`);
       
-      //   console.log(res.headers.raw());
-      //   console.log(res.headers.get('content-type'));
-    })
-    .catch(error =>{
-       c(error);
-    })
+//       //   console.log(res.headers.raw());
+//       //   console.log(res.headers.get('content-type'));
+//     })
+//     .catch(error =>{
+//        c(error);
+//     })
    
 
-   });
+//    });
 
-}
-//colocar una promesa
-const searchfileinDirectory = (directory) =>{
+// }
 
-const files = FileHound.create()
-.discard('node_modules')
-.paths(directory)
-.ext('md')
-.find();
 
-files
-.then(res =>{
-   //c(res);
-res.forEach((el, index)=> {
-   //imprimir los archivos con basename
-   c(`${index}: ${path.basename(el)}`);
-   arrayFile(el);
 
-});
-})
-.catch(error =>{
-   c(error);
-});
+// //colocar una promesa
+// const searchfileinDirectory = (directory) =>{
 
-}
+// const files = FileHound.create()
+// .discard('node_modules')
+// .paths(directory)
+// .ext('md')
+// .find();
+
+// files
+// .then(res =>{
+//    //c(res);
+// res.forEach((el, index)=> {
+//    //imprimir los archivos con basename
+//    c(`${index}: ${path.basename(el)}`);
+//    arrayFile(el)
+//    .then(res=>{
+      
+//          if (options.length === 0){
+//             //console log para ver los links
+//             //c(File);
+//             arrayLinks.forEach((el, index)=> {
+               
+//                c(`${index}: href: ${el.href}`);
+//                c(`   texto: ${el.text}`);
+//                c(`   file: ${el.file}`);
+            
+//             });
+//             }
+//             //funcion que muestra los link ok para validate o stats
+//             else if (options.length>0){
+//                     fetchlinks(arrayLinks);
+//                   }
+//       })
+//      .catch(error => c(error));
+
+// });
+// })
+// .catch(error =>{
+//    c(error);
+// });
+
+// }
--- mdLinks.js
+++ mdLinks.js
@@ -1,15 +1,161 @@
-let c = console.log;
 
-const mdLinks = (path, options) =>{
-return new Promise((resolved, rejected )=> {
-   if (path ===5 && options.length) {
-resolved(c("Hola aqui tus opciones"));
-c(options);
+const fs = require ('fs');
+const marked = require ('marked');
+const FileHound = require ('filehound');
+const path = require ('path');
+const fetch = require('node-fetch');
+
+const mdLinks = (path, options) => {
+let c = console.log;
+let arrayLinks =[];
+   fs.stat(path,(error, stats) => {
+
+      if (error){ 
+         c(error);
+      }
+      
+      if (stats.isFile()){
+      c("**************************************************");
+      c("*           Extrayendo links de un Archivo       *");
+      c("**************************************************");
+      
+      // llamar a la funcion para imprimir los links en la consola
+      arrayFile(path)
+      .then(res =>{
+         if (options.length === 0){
+            //console log para ver los links
+            //c(File);
+            // arrayLinks.forEach((el, index)=> {
+               
+            //    c(`${index}: href: ${el.href}`);
+            //    c(`   texto: ${el.text}`);
+            //    c(`   file: ${el.file}`);
+            
+            // });
+            c(arrayLinks);
+
+            }
+            //funcion que muestra los link ok para validate o stats
+            if (options.length>0){
+                    fetchlinks(path);
+                  }
+      })
+      .catch(error=> c(error));
+   
+      }
+      if (stats.isDirectory()){
+      c("***************************************************");
+      c("*        Extrayendo links de Un Directorio        *");
+      c("***************************************************");
+      searchfileinDirectory(path);
+      }
+      });
+
+
+//funcion para capturar los links
+const arrayFile =(path) =>{
+return new Promise ((resolved, rejected) => {
+fs.readFile(path,"utf-8", (error, data) =>{
+if (error){
+rejected (error);
+
 }
-else
-rejected(error); 
-    
+else{
+
+const renderer = new marked.Renderer();
+renderer.link =(href,title,text) =>{
+arrayLinks.push({
+href:href,
+text:text,
+file:path
 });
+}
+marked(data, {renderer:renderer});
+//condicional para imprimir sin options
+resolved(arrayLinks);
+}
+
+});
+});
+}
+
+//funcion para imprimir los links ok y no ok
+const fetchlinks = (arrayLinks) => {
+let arrayObjectFetch = [];
+arrayLinks.forEach(el => {
+   
+fetch(el.href)
+    .then(res => {
+       //c(res);
+       
+     c(`${res.url} ${res.ok} ${res.status} ${res.statusText}`);
+      
+      //   console.log(res.headers.raw());
+      //   console.log(res.headers.get('content-type'));
+    })
+    .catch(error =>{
+       c(error);
+    })
+   
+
+   });
+
+}
+
+
+//colocar una promesa
+const searchfileinDirectory = (directory) =>{
+
+const files = FileHound.create()
+.discard('node_modules')
+.paths(directory)
+.ext('md')
+.find();
+
+files
+.then(res =>{
+   //c(res);
+res.forEach((el, index)=> {
+   //imprimir los archivos con basename
+   // c(`${index}: ${path.basename(el)}`);
+   arrayFile(el)
+   .then(res=>{
+      
+      if (options.length === 0){
+         //console log para ver los links
+         //c(File);
+         // arrayLinks.forEach((el, index)=> {
+            
+         //    c(`${index}: href: ${el.href}`);
+         //    c(`   texto: ${el.text}`);
+         //    c(`   file: ${el.file}`);
+         
+         // });
+         c(arrayLinks);
+
+         }
+         //funcion que muestra los link ok para validate o stats
+         if (options.length>0){
+                 fetchlinks(path);
+               }
+      })
+     .catch(error => c(error));
+
+});
+})
+.catch(error =>{
+   c(error);
+});
+
+}
+
+
+   
+
+
+
+    
+
 }
   
 module.exports = mdLinks;
